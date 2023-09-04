d30f5dd4a62bef3ff1b49f4344ea5df8c3c6b85f
Archibald-prog <archpod@gmail.com>
2023-09-04 06:09:03 +0300

    Assigned the name 'home' to the url-pattern of the main page of the site.

--- peopledb/women/urls.py
+++ peopledb/women/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
 urlpatterns = [
-    path('', index),  # http://127.0.0.1:8000/women/
+    path('', index, name='home'),  # http://127.0.0.1:8000/women/
     path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/women/cats/
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2020/
 ]
