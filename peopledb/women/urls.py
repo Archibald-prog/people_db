ccb7a20c3e730016c00bd3c7f6827b86aae4ec47
Archibald-prog <archpod@gmail.com>
2023-09-04 05:10:45 +0300

    Added 'catid' parameter to 'categories' view and categories url pattern.

--- peopledb/women/urls.py
+++ peopledb/women/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index),  # http://127.0.0.1:8000/women/
-    path('cats/', categories),  # http://127.0.0.1:8000/women/cats/
+    path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/women/cats/
 ]
