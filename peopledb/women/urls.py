cbc8df242e5cae5f675c8b4546c6ac2331ffcc62
Archibald-prog <archpod@gmail.com>
2023-09-10 05:22:52 +0300

    Added url patterns for addpage, contact, login and show_post views. Deleted url-patterns for categories and archive views.

--- peopledb/women/urls.py
+++ peopledb/women/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from .views import *
 
 urlpatterns = [
     path('', index, name='home'),  # http://127.0.0.1:8000/women/
     path('about/', about, name='about'),
-    path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/women/cats/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2020/
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
