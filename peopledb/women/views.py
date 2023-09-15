c2993123b9785713ec7be72fb3b9dbd040fbcb2d
Archibald-prog <archpod@gmail.com>
2023-09-15 06:28:43 +0300

    Removed the 'menu' item from the context dictionary of all functions in women/views.py.

--- peopledb/women/views.py
+++ peopledb/women/views.py
@@ -3,18 +3,11 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-        ]
-
 
 def index(request):
     posts = Women.objects.all()
     context = {
         'posts': posts,
-        'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
     }
@@ -22,7 +15,10 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    context = {
+        'title': 'О сайте'
+    }
+    return render(request, 'women/about.html', context=context)
 
 
 def addpage(request):
@@ -53,7 +49,7 @@ def show_category(request, cat_id):
 
     context = {
         'posts': posts,
-        'menu': menu,
+        # 'menu': menu,
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id,
     }
